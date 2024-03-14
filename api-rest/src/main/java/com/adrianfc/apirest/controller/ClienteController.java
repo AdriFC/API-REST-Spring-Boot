a62533b6813330f1d05294e27356553ca23cfbc3
Adri <adri86vigo@gmail.com>
2024-03-14 19:31:39 +0100

    Aplicación de Http Status Code y personalización de validación con Response Status

--- api-rest/src/main/java/com/adrianfc/apirest/controller/ClienteController.java
+++ api-rest/src/main/java/com/adrianfc/apirest/controller/ClienteController.java
@@ -3,7 +3,14 @@ package com.adrianfc.apirest.controller;
 import com.adrianfc.apirest.model.entity.Cliente;
 import com.adrianfc.apirest.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,19 +20,33 @@ public class ClienteController {
     private ICliente clienteService;
 
     @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
     @PutMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente) {
        return clienteService.save(cliente);
     }
     @DeleteMapping("cliente/{id}")
-    public void delete(@PathVariable Integer id) {
-        Cliente clienteDelete = clienteService.findById(id);
-        clienteService.delete(clienteDelete);
+    public ResponseEntity <?> delete(@PathVariable Integer id) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            Cliente clienteDelete = clienteService.findById(id);
+            clienteService.delete(clienteDelete);
+            return new ResponseEntity<>(clienteDelete, HttpStatus.NO_CONTENT);
+        } catch (DataAccessException exDt) {
+            response.put("mensaje", exDt.getMessage());
+            response.put("cliente", null);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
     @GetMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Integer id) {
         return clienteService.findById(id);
     }
